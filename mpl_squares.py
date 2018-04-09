1c98c8d2ea6b168979d47a74f7059a3d9d13b272
kynyka <congsu.yeh@gmail.com>
2018-04-09 21:06:40 +0800

    修改标签文字和线条粗细

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,5 +4,14 @@ import matplotlib.pyplot as plt
 
 
 squares = [1, 4, 6, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)  # 设置绘制线条的粗细
+
+# 设置图表标题,并给坐标轴加上标签
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)  # x轴标题
+plt.ylabel('Square of Value', fontsize=14)  # y轴...
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
