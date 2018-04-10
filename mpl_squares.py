61855ed1c230469d0e9271fc2c89bbc3922ba979
kynyka <congsu.yeh@gmail.com>
2018-04-10 21:17:41 +0800

    scatter()绘制一系列点

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,7 +3,10 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)  # s为点的尺寸
 # 设置图表标题,并给坐标轴加上标签
 plt.title('Square Numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)  # x轴标题
