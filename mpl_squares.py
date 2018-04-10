c7e50b55991ee7744b4bfc7a705e09c910048d90
kynyka <congsu.yeh@gmail.com>
2018-04-10 21:00:43 +0800

    校正图形

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,8 +3,9 @@
 import matplotlib.pyplot as plt
 
 
+input_values = [1, 2, 3, 4, 5]  # 指定plot()的输入值;默认是第一个数据点的x坐标值为0
 squares = [1, 4, 6, 16, 25]
-plt.plot(squares, linewidth=5)  # 设置绘制线条的粗细
+plt.plot(input_values, squares, linewidth=5)  # 设置输入值/输出值/绘制线条的粗细
 
 # 设置图表标题,并给坐标轴加上标签
 plt.title('Square Numbers', fontsize=24)
