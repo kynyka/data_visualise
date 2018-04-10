c4d52c244f2f54a663437d41695bab051b4c931c
kynyka <congsu.yeh@gmail.com>
2018-04-10 21:25:31 +0800

    scatter(), 用循环自动计算数据

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,16 +3,16 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(xrange(1, 1001))
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)  # s为点的尺寸
+plt.scatter(x_values, y_values, s=40)
 # 设置图表标题,并给坐标轴加上标签
 plt.title('Square Numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)  # x轴标题
 plt.ylabel('Square of Value', fontsize=14)  # y轴...
 
-# 设置刻度标记的大小
-plt.tick_params(axis='both', which='major', labelsize=14)
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])  # x和y坐标轴的最小值和最大值
 
 plt.show()
