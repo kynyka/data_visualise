068361c02726ac458ebc30cb8c452be900a9112f
kynyka <congsu.yeh@gmail.com>
2018-04-11 13:38:25 +0800

    restruct fill_walk()

--- random_walk.py
+++ random_walk.py
@@ -19,13 +19,8 @@ class RandomWalk():
         # 不断漫步,直到列表达到指定的长度
         while len(self.x_values) < self.num_points:
             # 决定前进方向以及沿这个方向前进的距离
-            x_direction = choice([1, -1])  # 右1|左-1
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])  # 上1|下-1
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # 拒绝原地踏步
             if x_step == 0 and y_step == 0:
@@ -37,3 +32,9 @@ class RandomWalk():
 
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+
+
+    def get_step(self):
+        direction = choice([1, -1])  # 1 右/上|-1 左/下
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
