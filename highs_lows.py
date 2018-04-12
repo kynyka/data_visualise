e7dd30ab7c276e79d8766d811b39abd373b05a8c
kynyka <congsu.yeh@gmail.com>
2018-04-12 15:30:33 +0800

    print file headers and their position

--- highs_lows.py
+++ highs_lows.py
@@ -6,4 +6,6 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)  # 创建一个与被打开文件相关联的reader对象
     header_row = next(reader)  # 返回文件中的下一行
-    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
