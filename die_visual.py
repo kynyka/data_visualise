a07f4c554f42112f708aa636272ce1583129fa29
kynyka <congsu.yeh@gmail.com>
2018-04-12 13:47:49 +0800

    create histogram

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import pygal
 from die import Die
 
 
@@ -17,4 +18,13 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling one D6 1000 times.'
+hist.x_labels = ['1','2','3','4','5','6']
+hist.x_title = 'Result'
+hist.y_title = 'Frequency of Result'
+
+hist.add('D6', frequencies)
+hist.render_to_file('diagram/die_visual.svg')
