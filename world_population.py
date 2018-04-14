69cec625b5bc9bf735c306ce40d367cb46376935
kynyka <congsu.yeh@gmail.com>
2018-04-14 14:00:17 +0800

    draw the entire 2008 world population

--- world_population.py
+++ world_population.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import json
-
+import pygal.maps.world as pmw
 from country_codes import get_country_code
 
 # 将数据加载到一个列表中
@@ -8,13 +8,19 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+# 创建一个包含人口数量的字典
+cc_populations = {}
 # 打印每个国家2010年的人口数量
 for pop_dict in pop_data:
-    if pop_dict['Year'] == '2010':
+    if pop_dict['Year'] == '2008':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))  # 原始数据包含小数
         code = get_country_code(country_name)
         if code:
-            print(country_name + ': ' + str(population))
-        else:
-            print('ERROR - ' + country_name)
+            cc_populations[code] = population
+
+wm = pmw.World()
+wm.title = 'World Population in 2008, by Country'
+wm.add('2008', cc_populations)
+
+wm.render_to_file('diagram/world_population_2008.svg')
