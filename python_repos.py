752e7a20344bf7ebcf837082ce0f0aab9a3495b1
kynyka <congsu.yeh@gmail.com>
2018-04-15 13:45:34 +0800

    outline the most popular repos

--- python_repos.py
+++ python_repos.py
@@ -15,17 +15,10 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print('Repositories returned:', len(repo_dicts))
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
-# print('\nKeys:', len(repo_dict))  # 仔细查看这些键,可大致知道可提取有关项目的哪些信息(要准确地获悉API将返回哪些信息,要么阅读文档,要么像此处这样使用代码来查看这些信息【实际浏览器里打上地址看时就能了解信息了】)
-# for key in sorted(repo_dict.keys()):
-#     print(key)
-
 print('\nSelected information about first repository:')
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
